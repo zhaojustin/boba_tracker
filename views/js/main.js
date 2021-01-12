860082969dcc74cf4740d3cee1c5dda23b735b42
Justin Zhao <40444147+zhaojustin@users.noreply.github.com>
2021-01-11 19:19:33 -0800

    add spreadsheet link

--- views/js/main.js
+++ views/js/main.js
@@ -1,3 +1,9 @@
+//set money spent
 document.getElementById('money-spent').innerHTML = "$53"
-
+    //set boba count
 document.getElementById('boba-count').innerHTML = "10"
+
+function goToSpreadsheet() {
+    alert();
+    window.location.href = "https://docs.google.com/spreadsheets/d/1-0EqmqNetUNAlaTSyCAs5k_abydUEQeyM83AQzEKrbg/edit?usp=sharing";
+}
